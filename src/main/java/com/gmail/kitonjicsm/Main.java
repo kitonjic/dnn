cd9e658b0f1b5e53808a423e743da5c098a7bd28
Kitonjics Marko <kitonjic95@gmail.com>
2017-10-07 23:31:48 +0200

    changed num of layers in example

--- src/main/java/com/gmail/kitonjicsm/Main.java
+++ src/main/java/com/gmail/kitonjicsm/Main.java
@@ -6,7 +6,7 @@ import org.la4j.vector.dense.BasicVector;
 public class Main {
 
     public static void main(String[] args) {
-        Network network = new Network(new int[]{3, 2, 3, 5, 10, 2});
+        Network network = new Network(new int[]{3, 2, 3, 5, 2});
         BasicVector inputVector = new BasicVector(new double[]{0, 1, 0});
         network.feedForward(inputVector);
     }
